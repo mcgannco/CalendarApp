49591d0f196e6b416b36e303357361241e0489ee
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-19 19:51:28 -0400

    add homepage component

--- CalendarFrontEnd/components/app.jsx
+++ CalendarFrontEnd/components/app.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import HomeContainer from './home/home_container';
 
 const App = () => (
   <div>
     <h1>Calendar</h1>
+    <Switch>
+      <Route path="/" component={HomeContainer} />
+    </Switch>
   </div>
 );
 
