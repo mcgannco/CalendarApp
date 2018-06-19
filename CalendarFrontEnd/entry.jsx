a67f831db0d44f86ee4b964c6062efaa4389bbff
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-19 17:53:36 -0400

    initial setup

--- CalendarFrontEnd/entry.jsx
+++ CalendarFrontEnd/entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Root from './components/root'
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root />, root);
 });
