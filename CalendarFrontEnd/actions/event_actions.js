0082b00876c0a3e39b3e4cd63bc3104c0b6c82ec
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-26 19:16:18 -0400

    remove debugger

--- CalendarFrontEnd/actions/event_actions.js
+++ CalendarFrontEnd/actions/event_actions.js
@@ -46,7 +46,6 @@ export const requestAllEvents = () => dispatch => (
 )
 
 export const createEvent = event => dispatch => {
-  debugger
   return(
     APIUtil.createEvent(event).then(event => dispatch(receiveSingleEvent(event)),
   err => dispatch(receiveEventErrors(err.responseJSON))));
