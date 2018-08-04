51e0bed1d647483f7a592ca5a15f25a944ecd33a
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-08-04 18:16:58 -0400

    production ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  context: __dirname,
-  entry: "./CalendarFrontEnd/entry.jsx",
-  mode: 'production',
-  output: {
-     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['env', 'react']
-          }
-        },
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+    context: __dirname,
+    entry: './CalendarFrontEnd/entry.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
+                }
+            },
+        }]
+    },
+    devtool: 'source-map'
 };
